262b85f9ab856411d2c60917bdbbc48044de2829
yaseenmf37 <yaseenmf37@gmail.com>
2025-05-20 23:56:12 +0330

    up

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://samamassage.ir",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
