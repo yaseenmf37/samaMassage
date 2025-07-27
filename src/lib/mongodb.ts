c0a0c23be583fe282116a16dcafd44ed01e826ee
yaseenmf37 <yaseenmf37@gmail.com>
2025-07-27 12:33:55 +0330

    add mongodb

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -10,10 +10,19 @@ if (!MONGODB_URI) {
   );
 }
 
-let cached = global.mongoose;
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
 }
 
 async function connectDB() {
